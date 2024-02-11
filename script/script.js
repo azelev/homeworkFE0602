fae685b42732e979b140b0899f74bfa54965b886
Artur <zelevarthur@icloud.com>
2024-02-11 22:40:47 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,62 @@
+//Task 1
+
+/*
+for (let i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+*/
+
+//Task 2
+
+/*
+for (let i = 55; i >= 20; i--) {
+  console.log(i);
+}
+*/
+
+//Task 3
+
+/*
+const numbers = [3, 5, 11, 2, 8, 1, 6];
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+*/
+
+//Task 4 (метод map мы не учили, но гугл научит всему)
+
+/*
+const numbers = [3, 5, 11, 2, 8, 1, 6];
+
+const numbers_squared = numbers.map((number) => number ** 2);
+
+console.log(numbers_squared);
+*/
+
+//Task 5
+
+/*
+const numbers = [3, 5, 11, 2, 8, 1, 6];
+
+const numbers_squared = numbers.map((number) => number ** 2);
+
+const last_elem = numbers_squared[numbers_squared.length - 1];
+
+console.log(last_elem);
+*/
+
+//Task 6
+
+/*
+const user = {
+  first_name: "Ivan",
+  last_name: "Ivanov",
+  age: 20,
+  salary: 500,
+};
+
+const user_info = `User's name is ${user.first_name} ${user.last_name}. He is ${user.age} years old`;
+
+console.log(user_info);
+*/
